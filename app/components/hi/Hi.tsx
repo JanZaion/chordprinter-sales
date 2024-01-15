2e6994ada96e64f531d037e25154bb42a446979c
JanZaion <zajic815@gmail.com>
2024-01-15 20:43:30 +0100

    feat: add faulty comp

--- app/components/hi/Hi.tsx
+++ app/components/hi/Hi.tsx
@@ -1,7 +1,9 @@
 import styles from './Hi.module.css';
 
-export const Hi = () => (
-  <main className={styles.hi}>
-    hello world from chordprinter. Lets sell some plugins!
-  </main>
-);
+export const Hi = () => {
+  return (
+    <main className={styles.hi}>
+      hello world from chordprinter. Lets sell some plugins!
+    </main>
+  );
+};
