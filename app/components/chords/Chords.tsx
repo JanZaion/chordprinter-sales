d31923f71cfc8f8625329cf4761c48ec9341ec91
Adam Buryšek <114564710+AdamBurysek@users.noreply.github.com>
2024-01-15 23:10:10 +0100

    chord

--- app/components/chords/Chords.tsx
+++ app/components/chords/Chords.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import styles from './Chords.module.css';
+import './chords.css';
 
 const Chords = () => {
   return (
     <div className={styles.container}>
-      <span className={styles.note} />
+      <span></span>
+      <span></span>
+      <span></span>
     </div>
   );
 };
