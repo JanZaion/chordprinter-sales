75bc4384986a01fc3229276e514b9baefbb4b48d
JanZaion <zajic815@gmail.com>
2024-01-15 20:52:43 +0100

    add badadan

--- app/components/hi/Hi.tsx
+++ app/components/hi/Hi.tsx
@@ -2,6 +2,7 @@ import styles from './Hi.module.css';
 
 export const Hi = () => (
   <main className={styles.hi}>
+    <h1>Chordprinter made Badadan</h1>
     hello world from chordprinter. Lets sell some plugins!
   </main>
 );
