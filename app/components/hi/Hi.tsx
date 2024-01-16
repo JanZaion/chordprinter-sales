ee67e2e975d64215545b8ca887ace22451c50ef8
Adam Buryšek <114564710+AdamBurysek@users.noreply.github.com>
2024-01-17 00:39:01 +0100

    delete hi :(

--- app/components/hi/Hi.tsx
+++
@@ -1,7 +0,0 @@
-import styles from './Hi.module.css';
-
-export const Hi = () => (
-  <div className={styles.hi}>
-    <h1>ChordPrinter</h1>
-  </div>
-);
--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,13 @@
 import Chords from './components/chords/Chords';
-import { Hi } from './components/hi/Hi';
+import Hero from './components/hero/Hero';
 import styles from './page.module.css';
 import './globals.css';
-import Hero from './components/hero/Hero';
 
 const Home = () => (
   <main>
     <div className={styles.container}>
-      <Chords />
       <Hero />
+      <Chords />
       <div style={{ width: '100%', height: '5000px' }} />
     </div>
   </main>
