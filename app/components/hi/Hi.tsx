abf5bb581d80a4d135c4bc0e8b1a9ac746423bc4
JanZaion <zajic815@gmail.com>
2024-01-22 11:44:40 +0100

    fix: remove unused import

--- app/components/hi/Hi.tsx
+++ app/components/hi/Hi.tsx
@@ -1,7 +1,5 @@
-import styles from './Hi.module.css';
-
 export const Hi = () => (
-  <main className={styles.hi}>
+  <main>
     <h1>Chordprinter made Banan</h1>
     hello world from chordprinter. Lets sell some plugins!
   </main>
