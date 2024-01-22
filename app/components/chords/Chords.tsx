bab07defadf4331587624c236a204eb34a873357
Jan Zaion <62064996+JanZaion@users.noreply.github.com>
2024-01-22 11:50:18 +0100

    Logo - repull (#5)
    * banan
    
    * add note
    
    * chord
    
    * desktop chords done
    
    * added chords and background
    
    * banan and chords
    
    * eslint fix
    
    * add chords
    
    * add hero component and some global colors
    
    * delete hi :(
    
    * add light background
    
    * correctly imported font
    
    * eslint font import fix
    
    * fixed postioning, color and font weight
    
    * fix: remove unused import
    
    ---------
    
    Co-authored-by: Adam Buryšek <114564710+AdamBurysek@users.noreply.github.com>

---
+++ app/components/chords/Chords.tsx
@@ -0,0 +1,20 @@
+import styles from './Chords.module.css';
+
+const Chords = () => (
+  <div className={styles.container}>
+    <span />
+    <span />
+    <span />
+    <span />
+    <span />
+    <span />
+    <span />
+    <span />
+    <span />
+    <span />
+    <span />
+    <span />
+  </div>
+);
+
+export default Chords;
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,16 @@
-import { Hi } from './components/hi/Hi';
+import Chords from './components/chords/Chords';
+import Hero from './components/hero/Hero';
+import styles from './page.module.css';
+import './globals.css';
 
-const Home = () => <Hi />;
+const Home = () => (
+  <main>
+    <div className={styles.container}>
+      <Hero />
+      <Chords />
+      <div style={{ width: '100%', height: '5000px' }} />
+    </div>
+  </main>
+);
 
 export default Home;
