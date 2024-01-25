c71770ba3624cff84663d5fa57dbdf2c2c4db35c
Adam Buryšek <114564710+AdamBurysek@users.noreply.github.com>
2024-01-25 12:44:46 +0100

    change clamp to grid

--- app/components/hero/Hero.tsx
+++ app/components/hero/Hero.tsx
@@ -5,19 +5,22 @@ import HeroButtons from './HeroButtons';
 
 const Hero = () => (
   <div className={styles.container}>
-    <h1 className={styles.title}>ChordPrinter</h1>
-    <p className={styles.titleInfo}>Chord Progressions Just Became Easy</p>
-    <div className={styles.bottomContainer}>
-      <Image
-        alt="Chordprinter Image"
-        className={styles.chAdvImage}
-        src={chAdvImage}
-      />
-      <div className={styles.subtitle}>
-        Generate chord progressions instantly inside Ableton Live's clips.
-        Create loops that vibe with this Max for Live device!
+    <div className={styles.topContainer}>
+      <h1 className={styles.title}>ChordPrinter</h1>
+      <p className={styles.titleInfo}>Chord Progressions Just Became Easy</p>
+
+      <div className={styles.bottomContainer}>
+        <Image
+          alt="Chordprinter Image"
+          className={styles.chAdvImage}
+          src={chAdvImage}
+        />
+        <div className={styles.subtitle}>
+          Generate chord progressions instantly inside Ableton Live's clips.
+          Create loops that vibe with this Max for Live device!
+        </div>
+        <HeroButtons />
       </div>
-      <HeroButtons />
     </div>
   </div>
 );
