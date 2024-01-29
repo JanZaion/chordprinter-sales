a83845329d7738571103880c6ef992bb3d360474
Adam Buryšek <114564710+AdamBurysek@users.noreply.github.com>
2024-01-29 16:12:07 +0100

    hero done

--- app/components/hero/Hero.tsx
+++ app/components/hero/Hero.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image';
 import chAdvImage from '../../../public/images/chordprinterAdv.png';
-import HeroButtons from './HeroButtons';
+import chBasImage from '../../../public/images/chordprinterBasic.png';
 import Chords from './Chords';
 import styles from './Hero.module.css';
 import chordsStyles from './Chords.module.css';
@@ -13,15 +13,35 @@ const Hero = () => (
       <p className={styles.titleInfo}>Chord Progressions Just Became Easy</p>
       <div className={styles.bottomContainer}>
         <Image
-          alt="Chordprinter Image"
+          alt="Chordprinter Advanced Image"
           className={styles.chAdvImage}
           src={chAdvImage}
+        />
+        <Image
+          alt="Chordprinter Basic Image"
+          className={styles.chBasImage}
+          src={chBasImage}
         />
         <div className={styles.subtitle}>
           Generate chord progressions instantly inside Ableton Live's clips.
           Create loops that vibe with this Max for Live device!
         </div>
-        <HeroButtons />
+        <div className={styles.buttonContainer}>
+          <a
+            href="https://www.google.com/"
+            target="blank"
+            className={styles.button}
+          >
+            Try demo
+          </a>
+          <a
+            href="https://www.google.com/"
+            target="blank"
+            className={styles.button}
+          >
+            Buy
+          </a>
+        </div>
       </div>
     </div>
   </div>
