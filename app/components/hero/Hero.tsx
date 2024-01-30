65e40c11ef5163f397a6785c1d35e97ef0aa5201
Adam Buryšek <114564710+AdamBurysek@users.noreply.github.com>
2024-01-30 13:33:23 +0100

    fixing problems

--- app/components/hero/Hero.tsx
+++ app/components/hero/Hero.tsx
@@ -8,45 +8,49 @@ import chordsStyles from './Chords.module.css';
 import styles from './Hero.module.css';
 
 const Hero = () => (
-  <div className={styles.container}>
+  <>
     <Chords />
-    <div className={chordsStyles.container}>
-      <h1 className={styles.title}>ChordPrinter</h1>
-      <p className={styles.titleInfo}>Chord Progressions Just Became Easy</p>
-      <div className={styles.bottomContainer}>
-        <Image
-          alt="Chordprinter Advanced Image"
-          className={styles.chAdvImage}
-          src={chAdvImage}
-        />
-        <Image
-          alt="Chordprinter Basic Image"
-          className={styles.chBasImage}
-          src={chBasImage}
-        />
-        <div className={styles.subtitle}>
-          Generate chord progressions instantly inside Ableton Live&apos;s
-          clips. Create loops that vibe with this Max for Live device!
-        </div>
-        <div className={styles.buttonContainer}>
-          <a
-            className={styles.button}
-            href="https://www.google.com/"
-            target="blank"
-          >
-            Try demo
-          </a>
-          <a
-            className={styles.button}
-            href="https://www.google.com/"
-            target="blank"
-          >
-            Buy
-          </a>
+    <div className={styles.container}>
+      <div className={chordsStyles.container}>
+        <h1 className={styles.title}>ChordPrinter</h1>
+        <p className={styles.titleInfo}>Chord Progressions Just Became Easy</p>
+        <div className={styles.bottomContainer}>
+          <Image
+            alt="Chordprinter Advanced Image"
+            className={styles.chAdvImage}
+            src={chAdvImage}
+          />
+          <Image
+            alt="Chordprinter Basic Image"
+            className={styles.chBasImage}
+            src={chBasImage}
+          />
+          <div className={styles.subtitle}>
+            Generate chord progressions instantly inside Ableton Live&apos;s
+            clips. Create loops that vibe with this Max for Live device!
+          </div>
+          <div className={styles.buttonContainer}>
+            <a
+              className={styles.button}
+              href="https://www.google.com/"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              Try demo
+            </a>
+            <a
+              className={styles.button}
+              href="https://www.google.com/"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              Buy
+            </a>
+          </div>
         </div>
       </div>
     </div>
-  </div>
+  </>
 );
 
 export default Hero;
